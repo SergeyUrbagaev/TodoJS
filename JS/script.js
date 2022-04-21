10450ff9c570d16f3bed16d661a05c3fb4725b18
Сергей <urbagayev91@mail.ru>
2022-04-21 16:16:25 +0300

    finishToDoJS

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,32 @@
-const input = document.querySelector('#buy-bread');
-input.hasAttribute(name);
+let buttonAdd = document.querySelector('.button-add');
+let inputAdd = document.querySelector('.input-add');
+let taskBread = document.querySelector('.task-bread');
+let todoList = [];
+if (localStorage.getItem('todo')) {
+	todoList = JSON.parse(localStorage.getItem('todo'));
+	displayMessages();
+}
+buttonAdd.addEventListener('click', function () {
+	let newTodo = {
+		todo: inputAdd.value,
+		checked: false,
+		important: false
+	};
+	todoList.push(newTodo);
+	displayMessages();
+	localStorage.setItem('todo', JSON.stringify(todoList));
+});
 
+function displayMessages() {
+	let displayMessages = '';
+	todoList.forEach(function (item, i) {
+		displayMessages += `
+		<li>
+			<input type = 'checkbox' id = 'item_${i}' ${item.checked ? 'checked' : ''}>
+			<label for='item_${i}'>${item.todo}</label>
+		</li>
+		`;
+		taskBread.innerHTML = displayMessages;
+
+	});
+}
