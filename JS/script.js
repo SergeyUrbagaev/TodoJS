198549fc5aa6f50b5099272db0071f2c86709b49
Сергей <urbagayev91@mail.ru>
2022-08-10 22:12:22 +0300

    10082022

--- JS/script.js
+++ JS/script.js
@@ -7,59 +7,39 @@ let mass = [
 	{ id: 2, text: 'Купить хлеб', completed: false },
 	{ id: 3, text: 'Купить пиво', completed: false }
 ];
-let pustmass = [];
-// for (let i = 0; i < mass.length; i++) {
-// 	pustmass.push(mass[i]);
-// }
-// console.log(pustmass);
-function taskAdd(task) {
 
+function createNewElement(task) {
+	let li = document.createElement('li');
+	li.classList.add("task-item");
+
+	let checkbox = document.createElement('input');
+	checkbox.type = "checkbox";
+
+	let label = document.createElement('label');
+	label.innerText = task;
+
+	let redakt = document.createElement('button');
+	redakt.classList.add('redakt');
+	redakt.innerHTML = "<img src='image/red.png'>";
+
+	let delet = document.createElement('button');
+	delet.classList.add('delete');
+	delet.innerHTML = "<img src='image/del.png'>";
+
+	li.append(checkbox);
+	li.append(label);
+	li.append(redakt);
+	li.append(delet);
+	return li
+};
+
+function addTask() {
+	if (input.value != "") {
+		let li = createNewElement(input.value);
+		taskmeny.appendChild(li);
+	}
 }
-for (let i = 0; i < mass.length; i++) {
-	pustmass.push(mass[i].text);
-	taskmeny.innerHTML += '<li>' + + '</li>'
-}
-// //Создание таски
-// function createElem(task) {
-// 	var listItem = document.createElement('li');
-// 	listItem.className = 'task-item';
-// 	var checkbox = document.createElement('input')
-// 	checkbox.type = "checkbox";
-// 	checkbox.className = "custom-checkbox";
-// 	var label = document.createElement('label');
-// 	label.innerText = task;
-// 	var redakt = document.createElement("button");
-// 	redakt.className = "redakt";
-// 	redakt.innerHTML = "<img src='image/Group 8465.jpg'></img>";
-// 	var delet = document.createElement('button');
-// 	delet.className = "delete";
-// 	delet.innerHTML = '<img src="image/Group 8471.png">';
-// 	listItem.appendChild(checkbox);
-// 	listItem.appendChild(label);
-// 	listItem.appendChild(redakt);
-// 	listItem.appendChild(delet);
 
-// 	// Кнопка удаления
-// 	function deleteTask() {
-// 		let del = document.querySelectorAll('.delete');
-// 		delet.parentElement.remove();
-// 	};
-// 	delet.onclick = deleteTask;
-
-// 	return listItem
-// };
-// function addTask() {
-// 	if (input.value) {
-// 		var listItem = createElem(input.value);
-// 		taskmeny.appendChild(listItem);
-// 		input.value = "";
-// 	}
-// 	else {
-// 		alert('Введите задачу')
-// 	}
-// }
-// button.onclick = addTask;
-
-
+button.onclick = addTask;
 
 
